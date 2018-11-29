7a9a319d702a369535934bb161fd02f6e3fbe150
Tomislav Maras <tmaras@ymail.com>
2018-11-29 12:18:57 +0100

    Add 3rd party modules - Alertify, JWT, Ngx Bootstrap, Bootswatch

--- WestCore-SPA/src/app/app.module.ts
+++ WestCore-SPA/src/app/app.module.ts
@@ -10,6 +10,8 @@ import { AuthService } from './_services/auth.service';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptor, ErrorInterceptorProvider } from './_services/error.interceptor';
+import { AlertifyService } from './_services/alertify.service';
+import { BsDropdownModule } from 'ngx-bootstrap';
 
 @NgModule({
    declarations: [
@@ -22,11 +24,13 @@ import { ErrorInterceptor, ErrorInterceptorProvider } from './_services/error.in
       BrowserModule,
       AppRoutingModule,
       HttpClientModule,
-      FormsModule
+      FormsModule,
+      BsDropdownModule.forRoot()
    ],
    providers: [
       AuthService,
-      ErrorInterceptorProvider
+      ErrorInterceptorProvider,
+      AlertifyService
    ],
    bootstrap: [
       AppComponent
