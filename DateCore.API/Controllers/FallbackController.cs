3537dcf24fb2f3e01fe9ff40091071bf25f60f3e
Tomislav Maras <tmaras@ymail.com>
2018-12-22 22:30:03 +0100

    Configure prod build and static file serve

---
+++ DateCore.API/Controllers/FallbackController.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace DateCore.API.Controllers
+{
+    public class FallbackController : Controller
+    {
+        public IActionResult Index()
+        {
+            return PhysicalFile(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "index.html"), "text/HTML");
+        }
+     
+    }
+}
